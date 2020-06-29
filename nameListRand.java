996ce2bb2e83c4c99b4ef2e6f92ef7a934fa92c2
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2020-06-28 17:01:52 -0700

    intitial commit

---
+++ nameListRand.java
@@ -0,0 +1,45 @@
+import java.util.Random;
+import java.util.Vector;
+public class nameListRand {
+
+	public static void main(String[] args) {
+		
+		
+		Vector nameList = new Vector();
+		nameList.add("Shahbaz Singh Mansahia");
+		nameList.add("Deep");
+		nameList.add("Ahmed Mujtaba");
+		nameList.add("Azeez Temitope Olanrewaju");
+		nameList.add("Diming Chu");
+		nameList.add("Dongshuo Li");
+		nameList.add("Honorinicy");
+		nameList.add("Jason");
+		nameList.add("Junyu Liu");
+		nameList.add("Kellyq Liu");
+		nameList.add("Kinjan");
+		nameList.add("Kun Li");
+		nameList.add("Muhammed Umar");
+		nameList.add("Oaz");
+		nameList.add("PQ");
+		nameList.add("Prithviatani");
+		nameList.add("Richard");
+		nameList.add("Sahil Singh Sandhu");
+		nameList.add("Samuel Yang");
+		nameList.add("Scott (Shih-Kai) Jhou");
+		nameList.add("Shahriar Kabir");
+		nameList.add("Shakur");
+		nameList.add("Shrina Patel");
+		nameList.add("Siyu Qiu");
+		nameList.add("Siyulei");
+		nameList.add("Sumid");
+		nameList.add("abbytruong");
+		nameList.add("Tao");
+		nameList.add("Akshay Trivedi");
+		nameList.add("Yan Huang");
+		nameList.add("Saitong Zhao");
+		
+		System.out.println ("Hello");
+		//System.out.println ("Hello "+ nameList.elementAt(randNum));
+	}
+
+}
