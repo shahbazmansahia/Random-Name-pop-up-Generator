429a40cedc2bb1a7965a76b68c37e7ecb0bdf09f
shahbazmansahia <shahbaz_mansahia@yahoo.com>
2020-07-04 13:04:11 -0700

    removes names from list and asks for input again

--- nameListRand.java
+++ nameListRand.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Vector;
 import javax.swing.*;
 
@@ -6,10 +7,12 @@ public class nameListRand {
 
 	public static void main(String[] args) {
 		Random rand = new Random(System.currentTimeMillis());
-		
-		int randNum = Math.abs(rand.nextInt() % 31);
+		String ans = "";
+		Scanner scan = new Scanner (System.in);
+		boolean choice = true;
 		
 		Vector  <String> nameList = new Vector <String> ();
+		
 		nameList.add("Shahbaz Singh Mansahia");
 		nameList.add("Deep");
 		nameList.add("Ahmed Mujtaba");
@@ -42,11 +45,21 @@ public class nameListRand {
 		nameList.add("Yan Huang");
 		nameList.add("Saitong Zhao");
 		
-		String nameGenned = nameList.elementAt(randNum);
-		//System.out.println ("Hello");
-		//System.out.println ("Hello "+ nameList.size());
-		//System.out.println ("Hello "+ nameGenned);
-		JOptionPane.showMessageDialog (null, nameGenned.toUpperCase() + " HAS JOINED THE BATTLE!", "A new Challenger Appears!", 1);
+		while (choice) {
+			int randNum = Math.abs(rand.nextInt() % (nameList.size()));
+			
+			
+			String nameGenned = nameList.elementAt(randNum);
+			//System.out.println ("Hello");
+			//System.out.println ("Hello "+ nameList.size());
+			//System.out.println ("Hello "+ nameGenned);
+			JOptionPane.showMessageDialog (null, nameGenned.toUpperCase() + " HAS JOINED THE BATTLE!", "A new Challenger Appears!", 1);
+			nameList.remove(randNum);
+			System.out.println("Pick another name? y/n");
+			ans = scan.next();
+			choice = (ans.equals("y") || ans.equals("Y"))? true : false;
+		}
+		System.out.println("Exiting...");
 	}
 
 }
